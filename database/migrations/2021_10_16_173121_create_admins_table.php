28d2d6334627e488efc77191ca2fc54aa0713368
ANrajin <an.rajin@gmail.com>
2021-10-17 00:28:26 +0600

    db configured

--- database/migrations/2021_10_16_173121_create_admins_table.php
+++ database/migrations/2021_10_16_173121_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::connection('admin')->create('users', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->nullable();
+            $table->unsignedBigInteger('role_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('user_type', ['teacher', 'student', 'guest'])->default('guest');
             $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
