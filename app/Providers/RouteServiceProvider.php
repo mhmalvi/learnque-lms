5daca6f178ad6f327f1f12af8652df03b32e0ddd
ANrajin <dev.quadque@gmail.com>
2021-10-18 17:51:46 +0600

    admin authentication

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -48,7 +48,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
-            Route::middleware(['web', 'admin:admin', 'auth:admin'])
+            Route::middleware(['web', 'auth:admin', 'admin:admin'])
                 ->namespace($this->namespace)
                 ->prefix('admin')
                 ->name('admin.')
