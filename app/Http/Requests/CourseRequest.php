bbe6a68cab4916b489a708b4959e642deb799740
ANrajin <dev.quadque@gmail.com>
2021-11-02 18:02:46 +0600

    intervention added

--- app/Http/Requests/CourseRequest.php
+++ app/Http/Requests/CourseRequest.php
@@ -3,28 +3,55 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class CourseRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
+     * retrive file extension from base64 string
      */
-    public function authorize()
+    private function getOrigianlFileExtension($file)
     {
-        return false;
+        // $encodedImgString = explode(',', $file, 2)[1];
+        // $decodedImgString = base64_decode($encodedImgString);
+        // $info = getimagesizefromstring($decodedImgString);
+
+        // $ext = image_type_to_extension($info[2]);
+
+        // return $ext;
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
+     * store product thumbanil
+     * base64 encoded
      */
-    public function rules()
+    protected function storeThumbnailImages()
     {
-        return [
-            //
-        ];
+        // $file = $this->thumbnail;
+        // $name = $this->image_title ? Str::slug($this->image_title) : Str::slug($this->title);
+        // $extension = $this->getOrigianlFileExtension($file);
+        // $filename = "{$name}{$extension}";
+
+        // $this->makeDirectory();
+
+        // /**
+        //  * We will store the image first in our app storage
+        //  * Then transfer the file into ftp
+        //  * Then delete the file for app storage
+        //  */
+        // $image = Image::make($file)
+        //     ->fit(1000)
+        //     ->save(storage_path('app/public/temp/' . $filename));
+
+        // Storage::disk('ftp')->put(
+        //     'storage/products/' . $filename,
+        //     $image
+        // );
+
+        // Storage::delete("public/temp/" . $filename);
+
+        // return $filename;
     }
 }
