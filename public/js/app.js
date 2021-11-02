9452d40ef32928d7121de5c42f163f3c4da6628a
Zakaria <jakariablaine120@gmail.com>
2021-11-02 17:39:44 +0600

    add app.js

--- public/js/app.js
+++ public/js/app.js
@@ -28330,7 +28330,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.ql-container {\n  height: 300px !important;\n}\n.swal2-styled.swal2-confirm {\n  margin-right: 10px !important;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.ql-container {\r\n  height: 300px !important;\n}\n.swal2-styled.swal2-confirm {\r\n  margin-right: 10px !important;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
