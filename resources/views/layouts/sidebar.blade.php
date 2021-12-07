26e6b2cdca037a411bf1e1a7844319c228b091c3
Zakaria <jakariablaine120@gmail.com>
2021-12-07 15:35:22 +0600

    Remove avatar sidebar component temporarily

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,13 @@
 <div class="mdk-drawer__content">
     <div class="sidebar sidebar-dark-pickled-bluewood sidebar-left" data-perfect-scrollbar>
         <!-- Sidebar Content -->
-        <div>
-            <avatar-sidebar-component user_data="{{ auth()->user()->load('info') }}" />
-        </div>
+        <a href="javascript:void(0)" class="sidebar-brand ">
+            <span class="avatar avatar-xl sidebar-brand-icon h-auto">
+                <img class="sidebar-brand-icon rounded-circle"
+                    src="{{ asset('assets/images/256_rsz_nicolas-horn-689011-unsplash.jpg') }}" alt="Quadque - LMS">
+            </span>
+            <span>John Doe</span>
+        </a>
 
         <div class="sidebar-heading">User Type</div>
         <ul class="sidebar-menu">
