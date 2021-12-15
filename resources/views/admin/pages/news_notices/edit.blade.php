5cdfd0481ead0e0d7924d813c75fa83c27f5fb44
Zakaria <jakariablaine120@gmail.com>
2021-12-15 13:46:17 +0600

    Add return back button in page header

--- resources/views/admin/pages/news_notices/edit.blade.php
+++ resources/views/admin/pages/news_notices/edit.blade.php
@@ -2,6 +2,15 @@
 
 @section("title", "Edit News & Notices")
 
+@section('link')
+    <a href="{{ route('admin.news_and_notices.index') }}" class="btn btn-sm btn-outline-dark">
+        <span class="material-icons mr-1">
+            arrow_back
+        </span>
+        Return
+    </a>
+@endsection
+
 @section('content')
     <div class="container page__container">
         <div class="row">
