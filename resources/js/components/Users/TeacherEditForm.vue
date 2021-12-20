17d00b5f086f938aab5be011608c5e44448d665f
Zakaria <jakariablaine120@gmail.com>
2021-12-20 10:31:14 +0600

    Fix some problems in edit user forms

--- resources/js/components/Users/TeacherEditForm.vue
+++ resources/js/components/Users/TeacherEditForm.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <form @submit.prevent="handleUpdate">
+      <div class="col-lg-8 offset-lg-4">
+        <div class="alert alert-success" v-if="success_message">
+          {{ success_message }}
+        </div>
+        <div class="alert alert-danger" v-if="errors.message">
+          {{ errors.message }}
+        </div>
+      </div>
       <div class="row mb-32pt">
         <div class="col-lg-4">
           <div class="page-separator">
@@ -60,12 +68,6 @@
 
       <div class="row mb-32pt">
         <div class="col-md-8 offset-md-4">
-          <div class="alert alert-success" v-if="success_message">
-            {{ success_message }}
-          </div>
-          <div class="alert alert-danger" v-if="errors.message">
-            {{ errors.message }}
-          </div>
           <button
             type="submit"
             class="btn btn-primary"
